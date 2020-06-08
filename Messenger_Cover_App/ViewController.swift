e128ef1dc8854ba78907b7c0ad34876a996c6308
Thiện Tùng <tungcppro@gmail.com>
2020-06-08 10:58:57 +0700

    Getting started

---
+++ Messenger_Cover_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger_Cover_App
+//
+//  Created by Thien Tung on 6/8/20.
+//  Copyright © 2020 Thien Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
